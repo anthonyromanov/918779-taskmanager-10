229450324f400d28869aef050ddfa1e93b3b5fcc
Anton Romanov <609990@gmail.com>
2020-01-09 15:54:54 +0300

    Render page Fix

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const TASK_COUNT = 3;
 
 const createSiteMenuTemplate = () => {
@@ -587,7 +589,7 @@ render(taskListElement, createTaskEditTemplate(), `beforeend`);
 new Array(TASK_COUNT)
   .fill(``)
   .forEach(
-    () => render(taskListElement, createTaskEditTemplate(), `beforeend`)
+      () => render(taskListElement, createTaskTemplate(), `beforeend`)
   );
 
 const boardElement = siteMainElement.querySelector(`.board`);
